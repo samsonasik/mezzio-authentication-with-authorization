83a649fe8800b68de09a2266f2bd545b8036cb6a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2019-12-19 12:28:43 +0700

    fix namespace UserInterface session name

--- src/App/Handler/LoginPageHandler.php
+++ src/App/Handler/LoginPageHandler.php
@@ -15,7 +15,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Zend\Expressive\Authentication\UserInterface;
+use Mezzio\Authentication\UserInterface;
 
 class LoginPageHandler implements MiddlewareInterface
 {
