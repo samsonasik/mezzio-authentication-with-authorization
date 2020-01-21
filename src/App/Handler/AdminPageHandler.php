ea162ae8bb85402e204a0b87c009cef5337dcad9
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-01-21 19:42:02 +0700

    cs

--- src/App/Handler/AdminPageHandler.php
+++ src/App/Handler/AdminPageHandler.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace App\Handler;
 
-use Psr\Http\Server\RequestHandlerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Expressive\Template\TemplateRendererInterface;
 
