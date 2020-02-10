5982c1829cac0941eee8e0cb823e90faa2f95940
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-02-10 23:28:13 +0700

    add /api/ping test

---
+++ test/Integration/OpenPingPageTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppTest\Integration;
+
+use Laminas\Diactoros\ServerRequest;
+use Laminas\Diactoros\Uri;
+use Mezzio\Authentication\UserInterface;
+use PHPUnit\Framework\TestCase;
+
+class OpenPingPageTest extends TestCase
+{
+    private $app;
+
+    protected function setUp(): void
+    {
+        $this->app = AppFactory::create();
+    }
+
+    public function testOpenPingPageAsAguestGotOK()
+    {
+        $uri           = new Uri('/api/ping');
+        $serverRequest = new ServerRequest([], [], $uri);
+
+        $response = $this->app->handle($serverRequest);
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+}
