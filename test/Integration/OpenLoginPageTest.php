443f9db97974fc9f39792d57a81f68db30aa45d0
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-02-10 21:58:31 +0700

    add login page test

---
+++ test/Integration/OpenLoginPageTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppTest\Integration;
+
+use Laminas\Diactoros\ServerRequest;
+use Laminas\Diactoros\Uri;
+use Mezzio\Authentication\UserInterface;
+use PHPUnit\Framework\TestCase;
+
+class OpenLoginPageTest extends TestCase
+{
+    private $app;
+
+    protected function setUp()
+    {
+        $this->app = AppFactory::create();
+    }
+
+    /** @runInSeparateProcess */
+    public function testAsAuserRedirectToHomePage()
+    {
+        $sessionData                    = [
+            'username' => 'samsonasik',
+            'roles'    => [
+                'user',
+            ],
+        ];
+        $_SESSION[UserInterface::class] = $sessionData;
+
+        $uri           = new Uri('/login');
+        $serverRequest = new ServerRequest([], [], $uri);
+
+        $response = $this->app->handle($serverRequest);
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertEquals('/', $response->getHeaderLine('Location'));
+    }
+}
