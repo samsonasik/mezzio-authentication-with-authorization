7b6ba30bbc7b8ed3705c89da8d846389a956062b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-02-10 17:21:26 +0700

    try ob_end_clean()

--- test/Integration/BufferChecker.php
+++ test/Integration/BufferChecker.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace AppTest\Integration;
 
-use function ob_end_flush;
+use function ob_end_clean;
 use function ob_start;
 
 final class BufferChecker
 {
     public static function check()
     {
-        ob_end_flush();
+        ob_end_clean();
         ob_start();
     }
 }
