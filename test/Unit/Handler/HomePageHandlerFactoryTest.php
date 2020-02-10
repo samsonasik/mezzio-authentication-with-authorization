3b3dd0bf5f87d39dad6d589fa5b34fb1f3da8ecd
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-02-11 00:57:00 +0700

    unit test for factory handlers

---
+++ test/Unit/Handler/HomePageHandlerFactoryTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppTest\Unit\Handler;
+
+use App\Handler\HomePageHandler;
+use App\Handler\HomePageHandlerFactory;
+use Mezzio\Template\TemplateRendererInterface;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+
+use function get_class;
+
+class HomePageHandlerFactoryTest extends TestCase
+{
+    /** @var ContainerInterface|ObjectProphecy */
+    protected $container;
+
+    protected function setUp(): void
+    {
+        $this->container = $this->prophesize(ContainerInterface::class);
+    }
+
+    public function testFactoryWithTemplate()
+    {
+        $this->container
+            ->get(TemplateRendererInterface::class)
+            ->willReturn($this->prophesize(TemplateRendererInterface::class));
+
+        $factory = new HomePageHandlerFactory();
+
+        $homePage = $factory($this->container->reveal(), null, get_class($this->container->reveal()));
+
+        $this->assertInstanceOf(HomePageHandler::class, $homePage);
+    }
+}
