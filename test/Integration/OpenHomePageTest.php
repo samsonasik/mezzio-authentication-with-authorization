7d009c5c05e3ba1343beb95f55bd7754acb12713
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-02-10 21:25:46 +0700

    add test as a user open home page

--- test/Integration/OpenHomePageTest.php
+++ test/Integration/OpenHomePageTest.php
@@ -6,6 +6,7 @@ namespace AppTest\Integration;
 
 use Laminas\Diactoros\ServerRequest;
 use Laminas\Diactoros\Uri;
+use Mezzio\Authentication\UserInterface;
 use PHPUnit\Framework\TestCase;
 
 class OpenHomePageTest extends TestCase
@@ -25,5 +26,22 @@ class OpenHomePageTest extends TestCase
         $response = $this->app->handle($serverRequest);
         $this->assertEquals(302, $response->getStatusCode());
         $this->assertEquals('/login', $response->getHeaderLine('Location'));
+    }
+
+    public function testAsAuserGotOK()
+    {
+        $sessionData                    = [
+            'username' => 'samsonasik',
+            'roles'    => [
+                'user',
+            ],
+        ];
+        $_SESSION[UserInterface::class] = $sessionData;
+
+        $uri           = new Uri('/');
+        $serverRequest = new ServerRequest([], [], $uri);
+
+        $response = $this->app->handle($serverRequest);
+        $this->assertEquals(200, $response->getStatusCode());
     }
 }
