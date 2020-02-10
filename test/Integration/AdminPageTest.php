d11e2a517c5736a4d45e267cadddc9d325ab59dd
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-02-11 01:39:59 +0700

    retry

--- test/Integration/AdminPageTest.php
+++ test/Integration/AdminPageTest.php
@@ -9,6 +9,8 @@ use Laminas\Diactoros\Uri;
 use Mezzio\Authentication\UserInterface;
 use PHPUnit\Framework\TestCase;
 
+use function session_start;
+
 class AdminPageTest extends TestCase
 {
     private $app;
@@ -28,6 +30,25 @@ class AdminPageTest extends TestCase
         $this->assertEquals('/login', $response->getHeaderLine('Location'));
     }
 
+    public function testOpenAdminPageAsAnAdminGot200Ok()
+    {
+        session_start();
+
+        $sessionData                    = [
+            'username' => 'admin',
+            'roles'    => [
+                'admin',
+            ],
+        ];
+        $_SESSION[UserInterface::class] = $sessionData;
+
+        $uri           = new Uri('/admin');
+        $serverRequest = new ServerRequest([], [], $uri);
+
+        $response = $this->app->handle($serverRequest);
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
     public function testOpenAdminPageAsAuserGot403Forbidden()
     {
         $sessionData                    = [
@@ -43,24 +64,5 @@ class AdminPageTest extends TestCase
 
         $response = $this->app->handle($serverRequest);
         $this->assertEquals(403, $response->getStatusCode());
-    }
-
-    public function testOpenAdminPageAsAuserGot200Ok()
-    {
-        unset($_SESSION[UserInterface::class]);
-
-        $sessionData                    = [
-            'username' => 'admin',
-            'roles'    => [
-                'admin',
-            ],
-        ];
-        $_SESSION[UserInterface::class] = $sessionData;
-
-        $uri           = new Uri('/admin');
-        $serverRequest = new ServerRequest([], [], $uri);
-
-        $response = $this->app->handle($serverRequest);
-        $this->assertEquals(200, $response->getStatusCode());
     }
 }
