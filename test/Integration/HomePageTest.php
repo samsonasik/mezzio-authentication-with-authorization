a3d9d2c4464296b3912521acdd115b8748a37ba0
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-02-11 12:55:18 +0700

    test home page

---
+++ test/Integration/HomePageTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppTest\Integration;
+
+use Laminas\Diactoros\ServerRequest;
+use Laminas\Diactoros\Uri;
+use Mezzio\Authentication\UserInterface;
+use PHPUnit\Framework\TestCase;
+
+use function session_start;
+
+class HomePageTest extends TestCase
+{
+    private $app;
+
+    protected function setUp(): void
+    {
+        $this->app = AppFactory::create();
+    }
+
+    public function testOpenHomePageAsAguestRedirectToLoginPage()
+    {
+        $uri           = new Uri('/');
+        $serverRequest = new ServerRequest([], [], $uri);
+
+        $response = $this->app->handle($serverRequest);
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertEquals('/login', $response->getHeaderLine('Location'));
+    }
+
+    public function testOpenHomePageAsAuserGot200Ok()
+    {
+        $sessionData                    = [
+            'username' => 'samsonasik',
+            'roles'    => [
+                'user',
+            ],
+        ];
+        $_SESSION[UserInterface::class] = $sessionData;
+
+        $uri           = new Uri('/');
+        $serverRequest = new ServerRequest([], [], $uri);
+
+        $response = $this->app->handle($serverRequest);
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    protected function tearDown(): void
+    {
+        session_start();
+        unset($_SESSION[UserInterface::class]);
+    }
+}
