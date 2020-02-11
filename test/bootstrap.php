9ebaeb888058fc04317f24da41fa221c7dbdbf65
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-02-11 23:26:10 +0700

    debug

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -7,11 +7,13 @@ namespace AppTest;
 use AppTest\Integration\AppFactory;
 use Laminas\Diactoros\ServerRequest;
 use Laminas\Diactoros\Uri;
+use PDO;
 
 use function error_reporting;
 use function ini_set;
 use function preg_match;
 use function session_start;
+use function sprintf;
 
 use const E_ALL;
 
@@ -19,6 +21,20 @@ ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
 include 'vendor/autoload.php';
+
+$pdo = new PDO('pgsql:host=127.0.0.1;port=5432;dbname=postgres;user=postgres;password=postgres', null, null);
+//$pdo = new PDO('pgsql:host=127.0.0.1;port=5432;dbname=mezzio;user=developer;password=123456', null, null);
+//$pdo = new PDO('pgsql:host=127.0.0.1;port=5432;dbname=postgres;user=postgres;password=123456', null, null);
+
+$sql  = sprintf(
+    "SELECT %s FROM %s WHERE %s = :identity",
+    'password',
+    'users',
+    'username'
+);
+$stmt = $pdo->prepare($sql);
+
+exit;
 
 session_start();
 
