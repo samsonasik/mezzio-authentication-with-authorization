b609e3a1ca0d84bafd4d5319524eea8d69163c55
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-02-18 06:50:30 +0700

    add UserMiddlewareFactory test

---
+++ test/Unit/Middleware/UserMiddlewareFactoryTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppTest\Unit\Middleware;
+
+use App\Middleware\UserMiddleware;
+use App\Middleware\UserMiddlewareFactory;
+use Mezzio\Authentication\DefaultUserFactory;
+use Mezzio\Authentication\UserInterface;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+
+class UserMiddlewareFactoryTest extends TestCase
+{
+    /** @var ContainerInterface|ObjectProphecy */
+    protected $container;
+
+    protected function setUp(): void
+    {
+        $this->container = $this->prophesize(ContainerInterface::class);
+    }
+
+    public function testFactory()
+    {
+        $this->container
+            ->get(UserInterface::class)
+            ->willReturn(new DefaultUserFactory());
+
+        $this->container
+            ->get('config')
+            ->willReturn(
+                [
+                    'authentication' => [
+                        'redirect' => '/login',
+                    ],
+                ]
+            );
+
+        $factory    = new UserMiddlewareFactory();
+        $middleware = $factory($this->container->reveal());
+        $this->assertInstanceOf(UserMiddleware::class, $middleware);
+    }
+}
