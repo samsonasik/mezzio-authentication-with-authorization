3354c0d867a769de51ca2b138e77826a160a7438
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-02-25 21:16:47 +0700

    run both MySQL and PostgreSQL in Continues Integration

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -8,22 +8,7 @@ error_reporting(E_ALL);
 include 'vendor/autoload.php';
 
 if (getenv('CI') === 'Yes') {
-    $pdo = new PDO('pgsql:host=127.0.0.1;port=5432;dbname=postgres;user=postgres;password=postgres', null, null);
-    $pdo->exec(<<<SQL
-CREATE TABLE IF NOT EXISTS users(
-    username character varying(255) PRIMARY KEY NOT NULL,
-    password text NOT NULL,
-    role character varying(255) NOT NULL DEFAULT 'user'
-);
-
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
-INSERT INTO users(username, password, role) VALUES(
-    'samsonasik', crypt('123456', gen_salt('bf')), 'user'
-) ON CONFLICT(username) DO NOTHING;
-
-INSERT INTO users(username, password, role) VALUES(
-    'admin', crypt('123456', gen_salt('bf')), 'admin'
-) ON CONFLICT(username) DO NOTHING;
-SQL
-    ) || die(print_r($pdo->errorInfo(), true));
+    $config = include 'config/autoload/local.php';
+    $pdo = new PDO($config['authentication']['pdo']['dsn'], null, null);
+    $pdo->exec(file_get_contents(__DIR__ . '/Fixture/' . getenv('DBENGINE'))) || die(print_r($pdo->errorInfo(), true));
 }
