ab3b8ce5d82eda21c6434f2dc5c94b87816a7c1b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-02-25 22:18:32 +0700

    tweak

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -8,8 +8,8 @@ error_reporting(E_ALL);
 include 'vendor/autoload.php';
 
 if (getenv('CI') === 'Yes') {
-    $config = include 'config/autoload/local.php';
-    $pdo    = new PDO($config['authentication']['pdo']['dsn'], null, null);
+    $config = (include 'config/autoload/local.php')['authentication']['pdo'];
+    $pdo    = new PDO($config['dsn'], $config['username'], $config['password']);
     $pdo->exec(
         file_get_contents(__DIR__ . '/Fixture/' . getenv('DBENGINE') . '.sql')
     ) || die(print_r($pdo->errorInfo(), true));
